53ece2ff38c82d4dc780d05b527ddb6fad58840b
Ailsa Sun <135079815+ailsa-sun@users.noreply.github.com>
2024-06-23 00:36:22 -0700

    Update index.ino
    Finished the stuff

--- index.ino
+++ index.ino
@@ -10,6 +10,13 @@ double Kp = 2, Ki = 5, Kd = 1;
 int thermoDO = 4;
 int thermoCS = 5;
 int thermoCLK = 6;
+int time;
+int position = 0;
+double states[] = {150, 150, 255, 0};
+int newtime = 0;
+
+
+int state = 0;
 
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
@@ -20,7 +27,8 @@ void setup()
     pinMode(tc, INPUT);
     pinMode(output, OUTPUT);
 
-    setpoint = 100;
+
+    setpoint = states[position];
     myPID.SetMode(AUTOMATIC);
 
     Serial.println("MAX6675 test");
@@ -30,14 +38,37 @@ void setup()
 
 void loop()
 {
+    if (state == 1)
+    {
+       if (millis() - newtime > 60000)
+       {
+          state += 1;
+          setpoint = states[state];
+       }
+    }
+    else if (input >= states[state])
+    {
+       state += 1;
+       setpoint = states[state];
+       newtime = 0;
+    }
+
     input = (double)thermocouple.readCelsius();
-    // Serial.println(input);
+    
+    
     myPID.Compute();
-    Serial.print("input:");
-    Serial.println(input);
-    Serial.print("output:");
+
+    //printing position, input, and output
+    Serial.print(" position: ");
+    Serial.print(setpoint);
+    Serial.print(" input: ");
+    Serial.print(input);
+    Serial.print(" output: ");
     Serial.println(output);
 
+    //write it out
     analogWrite(outPin, output);
     delay(1000);
+
+    //Notes: 30 degrees past, it will turn on - 10 degrees celsius is not a big deal - ideas: we can build an offset by a constant
 }
